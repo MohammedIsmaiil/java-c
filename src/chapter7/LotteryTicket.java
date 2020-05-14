20a7b60368ae428f0e4b8e979ab63ad286d7d125
mohammed ismail <mohmed.ismaail@gmail.com>
2020-05-14 19:53:37 +0200

    edit sort function in main not in binary search

--- src/chapter7/LotteryTicket.java
+++ src/chapter7/LotteryTicket.java
@@ -6,15 +6,19 @@ We're going to create a Lottery Quick Pick application that will generate a lott
  but if any one of you uses this and wins the lottery,
 I do expect my cut. Okay?
  */
+import java.lang.reflect.Array;
+import java.util.Arrays;
 import java.util.Random;
 
 public class LotteryTicket {
 
     private static final int LENGTH = 6;
     private static final int MAX_TICKET_NUMBER = 69;
+
     public static void main(String[] args) {
 
         int[] tickets = generateNumbers();
+        Arrays.sort(tickets);
         printNumbers(tickets);
     }
 
@@ -22,14 +26,41 @@ public class LotteryTicket {
         int[] ticket = new int[LENGTH];
         Random random = new Random();
 
-        for (int i=0; i<6; i++){
-            ticket[i] = random.nextInt(MAX_TICKET_NUMBER) + 1;
+
+        for (int i=0; i<LENGTH; i++){
+            int randomNumber;
+                /*
+                    generate random number then search to make sure it doesn't
+                    already exist in the array. if it dose, regenerate and search again.
+                 */
+            do {
+                 randomNumber = random.nextInt(MAX_TICKET_NUMBER) + 1;
+                 System.out.println(randomNumber);
+            }while (search(ticket , randomNumber) );
+            // number is unique if you get here, add it to array
+            ticket[i] = randomNumber;
         }
         return ticket;
     }
 
+    /**
+     *
+     * @param array  array to search
+     * @param numberToSearchFor  value to search for
+     * @return true if found, false if not
+     */
+    public static boolean search(int[] array , int numberToSearchFor){
+        // Array must be sorted first
+//        Arrays.sort(array);
+        int index = Arrays.binarySearch(array, numberToSearchFor);
+        if (index >= 0){
+            return true;
+        }
+        else return false;
+    }
+
     public static void printNumbers(int[] tickets){
-        for (int i=0; i<6; i++){
+        for (int i=0; i<LENGTH; i++){
             System.out.print(tickets[i] + " | ");
         }
     }
