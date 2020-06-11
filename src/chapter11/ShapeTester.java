dc29e0687d7ba0b25f83cb8bab6eb77160842362
mohammed ismail <mohmed.ismaail@gmail.com>
2020-06-12 00:08:27 +0200

    chapter11 abstract

---
+++ src/chapter11/ShapeTester.java
@@ -0,0 +1,11 @@
+package chapter11;
+
+public class ShapeTester {
+    public static void main(String[] args) {
+        Shape rect = new Rectangle(10, 5);
+        rect.print();
+       System.out.println(rect.calculateArea());
+
+
+    }
+}
